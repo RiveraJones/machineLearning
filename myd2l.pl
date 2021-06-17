65839ca2079fee3b38a0d35d97fe56c5e6525ae5
fedora29 <oskar_skilled_@live.com>
2021-06-17 12:22:54 -0500

    aber

--- myd2l.pl
+++ myd2l.pl
@@ -62,3 +62,31 @@ $X = mx->nd->array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 
 p $X->sum(0, keepdims => 1)->aspdl
 p $X->sum(1, keepdims => 1)->aspdl
+
+sub softmax {
+    my($X) = @_;
+    $X_exp = exp($X);
+    $partion = $X_exp->sum(0, keepdims => 1);
+    return $X_exp / $partion;
+}
+
+$X = nd->random->normal(0, 1, shape=>[2, 5])
+$X_prob = softmax($X)
+$X_prob, $X_prob->sum(1)
+
+#-----------------------------------------------------------------------------------------#
+#3.6.3. Defining the Model
+#implement the softmax regression model.
+
+sub net{
+    my($X) = @_;
+    return softmax(mx->nd->dot($X.reshape([-1, $W.shape[0]]), $W) + $b)
+}
+
+#-----------------------------------------------------------------------------------------#
+#3.6.4. Defining the Loss Function
+
+$y = mx->nd->array([0, 2])
+$y_hat = mx->nd->array([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+$y_hat[[0, 1], y]
+ $x = mx->nd->array([1, 2, 4, 8])
